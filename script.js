d1540654f83c820ce166feb0e72b6c9d42bcfc67
Arslan Majeed <arslanmajeed214@gmail.com>
2023-07-15 02:26:31 +0500

    Fix linter related errors.

--- script.js
+++ script.js
@@ -7,55 +7,54 @@ class Book {
 
 class UI {
   static addBookToList(book) {
-    const list = document.getElementById("bookList");
-    const li = document.createElement("li");
+    const list = document.getElementById('bookList');
+    const li = document.createElement('li');
     li.innerHTML = `<div class='details'> <div> <strong> <span>"</span>${book.title}<span>"</span> by ${book.author} </strong> </div> <button class='delete'>Remove</button> </div>`;
     list.appendChild(li);
   }
 
   static clearInputFields() {
-    document.getElementById("titleInput").value = "";
-    document.getElementById("authorInput").value = "";
+    document.getElementById('titleInput').value = '';
+    document.getElementById('authorInput').value = '';
   }
 
   static showAlert(message, className) {
-    const div = document.createElement("div");
+    const div = document.createElement('div');
     div.className = `alert ${className}`;
     div.appendChild(document.createTextNode(message));
-    const container = document.querySelector("body");
-    const form = document.getElementById("bookForm");
+    const container = document.querySelector('body');
+    const form = document.getElementById('bookForm');
     container.insertBefore(div, form);
 
-    setTimeout(function () {
-      document.querySelector(".alert").remove();
+    setTimeout(() => {
+      document.querySelector('.alert').remove();
     }, 3000);
   }
 
   static deleteBookFromList(target) {
-    if (target.classList.contains("delete")) {
+    if (target.classList.contains('delete')) {
       target.parentElement.remove();
       const title = target.previousElementSibling.textContent;
-      const author =
-        target.previousElementSibling.previousElementSibling.textContent;
+      const author = target.previousElementSibling.previousElementSibling.textContent;
       const book = new Book(title, author);
       UI.removeFromLocalStorage(book);
-      UI.showAlert("Book removed", "success");
+      UI.showAlert('Book removed', 'success');
     }
   }
 
   static saveToLocalStorage(book) {
     let books = [];
-    if (localStorage.getItem("books")) {
-      books = JSON.parse(localStorage.getItem("books"));
+    if (localStorage.getItem('books')) {
+      books = JSON.parse(localStorage.getItem('books'));
     }
     books.push(book);
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   }
 
   static getBooksFromLocalStorage() {
     let books = [];
-    if (localStorage.getItem("books")) {
-      books = JSON.parse(localStorage.getItem("books"));
+    if (localStorage.getItem('books')) {
+      books = JSON.parse(localStorage.getItem('books'));
     }
     return books;
   }
@@ -70,31 +69,31 @@ class UI {
   static removeFromLocalStorage(book) {
     let books = UI.getBooksFromLocalStorage();
     books = books.filter(
-      (item) => item.title !== book.title || item.author !== book.author
+      (item) => item.title !== book.title || item.author !== book.author,
     );
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   }
 }
 
 // Event listeners
-document.getElementById("bookForm").addEventListener("submit", function (e) {
+document.getElementById('bookForm').addEventListener('submit', (e) => {
   e.preventDefault();
-  const title = document.getElementById("titleInput").value;
-  const author = document.getElementById("authorInput").value;
+  const title = document.getElementById('titleInput').value;
+  const author = document.getElementById('authorInput').value;
   const book = new Book(title, author);
-  if (title === "" || author === "") {
-    UI.showAlert("Please fill in all fields", "error");
+  if (title === '' || author === '') {
+    UI.showAlert('Please fill in all fields', 'error');
   } else {
     UI.addBookToList(book);
     UI.clearInputFields();
     UI.saveToLocalStorage(book);
-    UI.showAlert("Book added", "success");
+    UI.showAlert('Book added', 'success');
   }
 });
-document.getElementById("bookList").addEventListener("click", function (e) {
+document.getElementById('bookList').addEventListener('click', (e) => {
   UI.deleteBookFromList(e.target);
 });
 // Load books from local storage on page load
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', () => {
   UI.displayBooksFromLocalStorage();
 });

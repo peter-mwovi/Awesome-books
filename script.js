60609be978b633b14fef6505fff714c6727784be
Arslan Majeed <arslanmajeed214@gmail.com>
2023-07-15 21:12:22 +0500

    Complet content toggle functionality with respect to navbar

--- script.js
+++ script.js
@@ -7,54 +7,55 @@ class Book {
 
 class UI {
   static addBookToList(book) {
-    const list = document.getElementById('bookList');
-    const li = document.createElement('li');
+    const list = document.getElementById("bookList");
+    const li = document.createElement("li");
     li.innerHTML = `<div class='details'> <div> <strong> <span>"</span>${book.title}<span>"</span> by ${book.author} </strong> </div> <button class='delete'>Remove</button> </div>`;
     list.appendChild(li);
   }
 
   static clearInputFields() {
-    document.getElementById('titleInput').value = '';
-    document.getElementById('authorInput').value = '';
+    document.getElementById("titleInput").value = "";
+    document.getElementById("authorInput").value = "";
   }
 
   static showAlert(message, className) {
-    const div = document.createElement('div');
+    const div = document.createElement("div");
     div.className = `alert ${className}`;
     div.appendChild(document.createTextNode(message));
-    const container = document.querySelector('body');
-    const form = document.getElementById('bookForm');
+    const container = document.querySelector("body");
+    const form = document.getElementById("bookForm");
     container.insertBefore(div, form);
 
     setTimeout(() => {
-      document.querySelector('.alert').remove();
+      document.querySelector(".alert").remove();
     }, 3000);
   }
 
   static deleteBookFromList(target) {
-    if (target.classList.contains('delete')) {
+    if (target.classList.contains("delete")) {
       target.parentElement.remove();
       const title = target.previousElementSibling.textContent;
-      const author = target.previousElementSibling.previousElementSibling.textContent;
+      const author =
+        target.previousElementSibling.previousElementSibling.textContent;
       const book = new Book(title, author);
       UI.removeFromLocalStorage(book);
-      UI.showAlert('Book removed', 'success');
+      UI.showAlert("Book removed", "success");
     }
   }
 
   static saveToLocalStorage(book) {
     let books = [];
-    if (localStorage.getItem('books')) {
-      books = JSON.parse(localStorage.getItem('books'));
+    if (localStorage.getItem("books")) {
+      books = JSON.parse(localStorage.getItem("books"));
     }
     books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
+    localStorage.setItem("books", JSON.stringify(books));
   }
 
   static getBooksFromLocalStorage() {
     let books = [];
-    if (localStorage.getItem('books')) {
-      books = JSON.parse(localStorage.getItem('books'));
+    if (localStorage.getItem("books")) {
+      books = JSON.parse(localStorage.getItem("books"));
     }
     return books;
   }
@@ -69,31 +70,53 @@ class UI {
   static removeFromLocalStorage(book) {
     let books = UI.getBooksFromLocalStorage();
     books = books.filter(
-      (item) => item.title !== book.title || item.author !== book.author,
+      (item) => item.title !== book.title || item.author !== book.author
     );
-    localStorage.setItem('books', JSON.stringify(books));
+    localStorage.setItem("books", JSON.stringify(books));
   }
 }
 
 // Event listeners
-document.getElementById('bookForm').addEventListener('submit', (e) => {
+document.getElementById("bookForm").addEventListener("submit", (e) => {
   e.preventDefault();
-  const title = document.getElementById('titleInput').value;
-  const author = document.getElementById('authorInput').value;
+  const title = document.getElementById("titleInput").value;
+  const author = document.getElementById("authorInput").value;
   const book = new Book(title, author);
-  if (title === '' || author === '') {
-    UI.showAlert('Please fill in all fields', 'error');
+  if (title === "" || author === "") {
+    UI.showAlert("Please fill in all fields", "error");
   } else {
     UI.addBookToList(book);
     UI.clearInputFields();
     UI.saveToLocalStorage(book);
-    UI.showAlert('Book added', 'success');
+    UI.showAlert("Book added", "success");
   }
 });
-document.getElementById('bookList').addEventListener('click', (e) => {
+document.getElementById("bookList").addEventListener("click", (e) => {
   UI.deleteBookFromList(e.target);
 });
 // Load books from local storage on page load
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   UI.displayBooksFromLocalStorage();
 });
+
+let bookListI = document.getElementById("book-container");
+let contactI = document.getElementById("contact");
+let bookFormI = document.getElementById("bookForm");
+
+function showContent(itemId) {
+  var item = document.getElementById(itemId);
+
+  if (itemId === "book-container") {
+    bookListI.classList.remove("hidden");
+    contactI.classList.add("hidden");
+    bookFormI.classList.add("hidden");
+  } else if (itemId === "contact") {
+    bookListI.classList.add("hidden");
+    contactI.classList.remove("hidden");
+    bookFormI.classList.add("hidden");
+  } else {
+    bookListI.classList.add("hidden");
+    contactI.classList.add("hidden");
+    bookFormI.classList.remove("hidden");
+  }
+}
